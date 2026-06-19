c6a813816558d4839cdba1f71e5a94280ef57a1f
tlming <2793303546@qq.com>
2026-06-20 00:12:53 +0800

    补充对posts的API的业务封装

--- api/endpoints/posts.py
+++ api/endpoints/posts.py
@@ -4,35 +4,47 @@ class PostsAPI:
     def __init__(self, client):
         self.client = client
 
-    def postlist(self) -> list:
+    def post_list(self) -> list:
         """GET /posts —— 列出所有 posts。"""
-        return self.client.get('/posts').json()
+        return self.client.get('/posts')
 
-    def get(self, post_id) -> dict:
+    def get_by_userId(self, user_id)->dict:
         '''
-        GET/posts/{post_id} —— 获取单个post接口信息
+        GET /posts?userId={userId} —— 通过userId获取post接口信息
         '''
-        return self.client.get(f'/posts/{post_id}').json()
+        return self.client.get(f'/posts?userId={user_id}')
+
+    def get_by_id(self, post_id) -> dict:
+        '''
+        GET/posts/{post_id} —— 通过id获取单个post接口信息
+        '''
+        return self.client.get(f'/posts/{post_id}')
+
+    def get_post_comments(self, post_id) -> list:
+        '''
+        GET /posts/{id}/comments —— 通过id获取对于title的评论列表
+        '''
+        return self.client.get(f'/posts/{post_id}/comments')
 
     def create(self, user_id: int, title: str, body: str) -> dict:
         '''
         POST/posts —— 创建post接口
         '''
-        return self.client.post('/posts', json={"userId": user_id, "title": title, "body": body}).json()
+        return self.client.post('/posts', json={"userId": user_id, "title": title, "body": body})
 
     def update(self, post_id: int,user_id: int, title: str, body: str) -> dict:
         '''
         PUT/posts/{id} ——更新接口全部信息
         '''
-        return self.client.put(f'/posts/{post_id}', json={"userId": user_id, "title": title, "body": body}).json()
+        return self.client.put(f'/posts/{post_id}', json={"userId": user_id, "title": title, "body": body})
 
-    def patch(self, post_id: int,**kwargsr) -> dict:
+    def patch(self, post_id: int,**kwargs) -> dict:
         '''
         PATCH /posts/{id}  ——更新接口部分信息
         '''
-        return  self.client.patch(f'/posts/{post_id}', json=kwargsr).json()
+        return  self.client.patch(f'/posts/{post_id}', json=kwargs)
     def delete(self, post_id: int) -> dict:
         '''
         DELETE /posts/{id} ——删除接口信息
         '''
-        return self.client.delete(f'/posts/{post_id}').json()
+        return self.client.delete(f'/posts/{post_id}')
