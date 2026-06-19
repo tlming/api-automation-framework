570d0e2f6e41ef44b1fcbec9cb10f716f178360f
tlming <2793303546@qq.com>
2026-06-20 00:16:20 +0800

    修改_request方法，返回所有响应内容（之前是只返回响应中的响应体）

--- api/client.py
+++ api/client.py
@@ -33,8 +33,13 @@ class ApiClient:
         logger.debug(f'request kwargs:{kwargs}')
         try:
             resp = self.session.request(method, url, timeout=self.timeout, **kwargs)
-            resp.raise_for_status()
-            # 只要上面没报错，说明请求成功，直接在这里返回
+            # 根据状态码分类记录日志
+            if 200 <= resp.status_code < 300:
+                logger.info(f"请求成功: {resp.status_code}")  # 绿
+            elif 400 <= resp.status_code < 500:
+                logger.warning(f"客户端错误（请求参数/权限问题）: {resp.status_code}")  # 黄
+            elif resp.status_code >= 500:
+                logger.error(f"服务器内部错误: {resp.status_code}")  # 红
             return resp
         except requests.exceptions.RequestException as e:
             logger.error(f"请求出错: {e}")  # 打日志
