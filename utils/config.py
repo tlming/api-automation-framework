256903edff9049b771b6dd13bc9373ab222c6ded
tlming <2793303546@qq.com>
2026-06-22 23:26:58 +0800

    增加获取token的环境变量

--- utils/config.py
+++ utils/config.py
@@ -7,5 +7,9 @@ load_dotenv('.env.test')
 class Config:
     base_url = os.getenv('BASE_URL')
     timeout = int(os.getenv('TIMEOUT'))
+    login_url = os.getenv('LOGIN_URL')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    token_buffer_seconds = int(os.getenv('TOEKN_BUFFER_SECONDS'))
 
 config = Config()
