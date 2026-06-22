2a8eaf1be64b6bab803b33bd265467a79113ce67
tlming <2793303546@qq.com>
2026-06-22 16:43:05 +0800

    修复文件引用bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from utils.config import config
 from utils.logger import setup_logger
-from notification import send_serverchan_message
-from jenkins_utils import is_running_in_jenkins
+from utils.notification import send_serverchan_message
+from utils.jenkins_utils import is_running_in_jenkins
 
-__all__=['config','setup_logger',send_serverchan_message,is_running_in_jenkins]
+__all__ = ['config', 'setup_logger', 'send_serverchan_message', 'is_running_in_jenkins']
